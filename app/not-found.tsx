27a1bfeb24b8ab1c3ebc5f1e142dcb938f16e627
surafel <surafelmengist2015@gmail.com>
2026-02-01 19:51:59 +0300

    add custom error

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound(){
+    return (
+        <div className="text-red-700 text-center p-4 bg-indigo-600 w-90 mx-auto min-h-screen flex flex-col items-center justify-center border border-gray-300 rounded  ">
+
+            <p>Page not found</p>
+            <p>404: Page not found please fix the server errors</p>
+        </div>
+    )
+}
