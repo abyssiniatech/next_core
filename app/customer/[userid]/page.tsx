bc7f2d36e40b73f20f3d5a46f74e9439bf06124d
surafel <surafelmengist2015@gmail.com>
2026-02-01 14:39:31 +0300

    add daynamic page inside the app

---
+++ app/customer/[userid]/page.tsx
@@ -0,0 +1,20 @@
+// app/customer/[userid]/page.tsx
+type PageProps = {
+  params: {
+    userid: string;
+  };
+};
+
+export default function CustomerDetailPage({ params }: PageProps) {
+  const { userid } = params;
+
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-2">Customer Details</h1>
+
+      <p className="text-lg">
+        Customer ID: <span className="font-semibold">{userid}</span>
+      </p>
+    </div>
+  );
+}
