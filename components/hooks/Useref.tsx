1d8a0725dbb67a0a317b115951bc85eefe092e67
surafel <surafelmengist2015@gmail.com>
2026-02-03 00:06:45 +0300

    add useref hook in newxt app

--- components/hooks/Useref.tsx
+++ components/hooks/Useref.tsx
@@ -1,3 +1,6 @@
+
+   "use client"
+
 import React, { useRef } from 'react'
 
 const Useref = () => {
@@ -6,7 +9,7 @@ const Useref = () => {
     <div className='m-4 p-4 border-2 border-blue-400'>
         <form className='flex flex-col space-y-4'>
           <input className='border-2 border-gray-300 p-2 rounded-md' type="text" ref={inputRef} />
-          <button onClick={() => inputRef.current?.focus()}>Focus</button>
+          <button onClick={() => inputRef.current?.focus()} className='bg-blue-500 text-white p-2 rounded-md'>Focus</button>
         </form>
     </div>
   )
