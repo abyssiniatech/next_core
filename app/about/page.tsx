d3c56d8a6319232ab0a03f80926facb249733fe3
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-06 16:14:53 +0300

    add fetch data

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,7 @@
 export default function(){
-    return <h1>Welcome to my channel</h1>
+    return(
+        <div className="min-h-screen bg-gradient-to-br from-purple-900 to-pink-800 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-center mt-8">About Page</h1>
+    </div>
+    ) 
 }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,13 +1,21 @@
+interface blog{
+    id:number
+    title:string
+    completed:boolean
+}
+
 export default async function BlogApi(){
     const response= await fetch("https://jsonplaceholder.typicode.com/todos")
     const data = await response.json()
     return(
         <div>
             {
-                data.map((val)=>{
+                data.map((val: blog)=>{
                     return(
-                        <section>
-                            <p></p>
+                        <section key={val.id}>
+                            <p>{val.id}</p>
+                            <p>{val.title}</p>
+                            <p>{val.completed}</p>
                         </section>
                     )
                 })
