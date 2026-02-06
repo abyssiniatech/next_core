601d74382be18ce70f73242c3650b1cbbe2de84f
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-06 16:45:26 +0300

    initial commit about the day 41

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
-import Navlink from "@/Day40/Navlink";
-
+import NavigationLink from "@/day41/Navlink";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navlink />
+        <NavigationLink />
         {children}
         <Footer />
       </body>
---
+++ day41/Navlink.tsx
@@ -0,0 +1,19 @@
+  "use client"
+
+import Link from "next/link"
+
+const NavigationLink = () => {
+  return (
+    <div className="bg-gradient-to-r from-purple-500 to-pink-500 p-4 flex items-center justify-between ">
+         <h2 className="text-white font-bold mr-4">My website</h2>
+      <nav className="flex justify-center items-center gap-4">
+         <Link href = "/">Home</Link>
+         <Link href = "/about">About</Link>
+         <Link href = "/contact">Contact</Link>
+         
+      </nav>
+    </div>
+  )
+}
+
+export default NavigationLink
