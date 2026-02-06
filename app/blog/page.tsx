62d50ff3ec9e5171fb9a2ea86ffbd7876c5d78e3
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-06 17:34:10 +0300

    add daynamic by using params

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,25 +1,11 @@
-interface blog{
-    id:number
-    title:string
-    completed:boolean
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>Blog page!</h1>
+    </div>
+  )
 }
 
-export default async function BlogApi(){
-    const response= await fetch("https://jsonplaceholder.typicode.com/todos")
-    const data = await response.json()
-    return(
-        <div>
-            {
-                data.map((val: blog)=>{
-                    return(
-                        <section key={val.id}>
-                            <p>{val.id}</p>
-                            <p>{val.title}</p>
-                            <p>{val.completed}</p>
-                        </section>
-                    )
-                })
-            }
-        </div>
-    )
-}
+export default page
