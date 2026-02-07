1ce18f0a67fef212ca4ee594cd1015165df1fef0
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-07 13:08:57 +0300

    add custome fetch api

--- day42/Marketing.tsx
+++ day42/Marketing.tsx
@@ -1,15 +1,15 @@
-      "use client"
-      
+    "use client"
+
   import Link from "next/link"
   export default function Marketinglayout(){
     return(
-        <div className="bg-indigo-300 min-h-screen text-white text-2xl font-bold p-10 flex justify-center items-center flex-col gap-10">
+        <div className="bg-indigo-300  text-white  font-bold p-4 flex justify-between items-center  gap-4">
          <h1>My website</h1>
          <nav className="flex justify-between gap-4">
             <Link href="/" >Home </Link>
             <Link href="/about" > About</Link>
             <Link href="/product" >Product</Link>
-            <Link href="/marketing" >Marketing</Link>
+            <Link href="/market" >Market</Link>
          </nav>
        </div>
     )
