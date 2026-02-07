5f7f6e5be48a7c56fd630176fdf8cc8509a2fc3e
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-07 13:49:44 +0300

    add active link in the nav section

--- day42/Marketing.tsx
+++ day42/Marketing.tsx
@@ -1,15 +1,25 @@
     "use client"
-
+  import { usePathname } from "next/navigation"
   import Link from "next/link"
+  const Navlinks=[
+    {name:"home",href:"/"},
+    {name:"About",href:"/about"},
+    {name:"Product",href:"/product"},
+    {name:"market",href:"/market"},
+  ]
+
+
+
   export default function Marketinglayout(){
+    const pathname=usePathname()
     return(
         <div className="bg-indigo-300  text-white  font-bold p-4 flex justify-between items-center  gap-4">
          <h1>My website</h1>
          <nav className="flex justify-between gap-4">
-            <Link href="/" >Home </Link>
-            <Link href="/about" > About</Link>
-            <Link href="/product" >Product</Link>
-            <Link href="/market" >Market</Link>
+           {Navlinks.map((link)=>{
+            const isActive=pathname===link.href
+            return(<Link href={link.href} key={link.name} className={isActive?"bg-white text-pink-800":"bg-indigo-300   text-blue-700"}>{link.name}</Link>)
+           })}
          </nav>
        </div>
     )
