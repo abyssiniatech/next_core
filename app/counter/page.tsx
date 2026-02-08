cc937bf89675f67ceb0903ff5d44a24835279fc8
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-08 14:42:24 +0300

    add custom Links

--- app/counter/page.tsx
+++ app/counter/page.tsx
@@ -1,8 +1,14 @@
 "use client"
 
-
 import {useState} from "react"
 import {useRouter} from "next/navigation"
+import Link from "next/link";
+const NavLinks=[
+    {name:"Home",path:"/"},
+    {name:"About",path:"/about"},
+    {name:"Counter",path:"/counter"},
+    {name:"Contact",path:"/contact"},
+]
 
 export default function page(){
     const [count, setCount] = useState<number>(0);
@@ -33,6 +39,18 @@ export default function page(){
              <section className="bg-indigo-700 w-100 h-80 rounded py-10 px-4 flex items-center justify-center">
                 <button className="bg-pink-800 text-white px-4 py-2 rounded" onClick={handleclick}>Go to home page</button>
              </section>
+             
+              <nav className="flex gap-4 rounded p-4 bg-gray-100">
+      {NavLinks.map((link) => (
+        <Link
+          key={link.name}
+          href={link.path}
+          className="text-blue-600 hover:text-blue-800"
+        >
+          {link.name}
+        </Link>
+      ))}
+    </nav>
         </div>
     );
 }
