0efe5d7f0d7cb2186cc85fb092c7562562b357a2
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-09 17:39:41 +0300

    add shadcn card in the projectr

--- app/shadcn/page.tsx
+++ app/shadcn/page.tsx
@@ -1,15 +1,53 @@
-import React from "react";
-import { Calendar } from "../../components/ui/calendar"; // Update this path if your Calendar component is elsewhere
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+const page = () => {
+  return (
+    <div className=" max-w-md p-2 mx-auto flex flex-col items-center justify-center gap-4 border border-gray-400 m-2 rounded bg-gray-100 min-h-screen">
+       <section className="w-auto">
+      <h1 className="text-2xl font-bold">Shadcn Accordion</h1>
+      <Accordion type="single" collapsible defaultValue="item-1">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+      </section>
+      
+      <section className="bg-indigo-900 w-auto p-4 rounded">
+      <h1 className="text-2xl font-bold">Shadcn card</h1>
+      <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+            <CardAction>Card Action</CardAction>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+      </section>
 
-export default function Dateshadcn() {
-    const [date, setDate] = React.useState<Date | undefined>(new Date());
 
-    return (
-        <Calendar
-            mode="single"
-            selected={date}
-            onSelect={setDate}
-            className="rounded-lg border"
-        />
-    );
+    </div>
+  )
 }
+
+export default page
--- components/hooks/todo/TodoApp.tsx
+++ components/hooks/todo/TodoApp.tsx
@@ -1,37 +1,10 @@
-    "use client"
-    import {SetStateAction, useState}  from "react"
+
 const TodoApp = () => {
-    const [todo,setTodo] = useState<string>('')
-    const [todos,setTodos] = useState <string[]>([])
-    const handleChange =(e: { target: { value: SetStateAction<string>; }; }) =>{
-        setTodo(
-             e.target.value;
-        )
-    }
-       
-
-const handleSubmit =(e) =>{
-      e.preventDafault()
-      setTodos(
-        [...todos,todo]
-      )
-}
-    return (
+  return (
     <div>
-         <header>
-
-         <h1>todo app</h1>
-         <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="enter todo" onChange={handleChage}/>
-            <button>Add</button>
-         </form>
-         </header>
+      <h1>hello these is the best</h1>
     </div>
   )
 }
 
 export default TodoApp
-function setTodos(arg0: any[]) {
-    throw new Error("Function not implemented.");
-}
-
