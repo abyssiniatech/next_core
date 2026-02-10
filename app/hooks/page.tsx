04ab92ab76313f7deb845d11eb9a4bbf59ff522a
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-10 11:58:32 +0300

    add usereducerhook in next app

--- app/hooks/page.tsx
+++ app/hooks/page.tsx
@@ -8,6 +8,7 @@ import Card from '@/components/hooks/Card'
 import Usestatehook from '@/components/hooks/Usestatehook'
 import Useeffecthook from '@/components/hooks/Useeffecthook'
 import Userefhook from '@/components/hooks/Userefhook'
+import Usereducerhook from '@/components/hooks/Usereducerhook'
 const page = () => {
   return (
     <div className='text-center min-h-screen text-pink-900 flex-col flex items-center justify-center '>
@@ -21,6 +22,7 @@ const page = () => {
       <Usestatehook />
       <Useeffecthook />
       <Userefhook />
+      <Usereducerhook />
     </div>
   )
 }
