17212b0d9c45d3e59607e00a49bf1a11959dd9d5
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-11 18:55:26 +0300

    add react-hook-form

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,13 +1,14 @@
 import Simpleform from "@/components/Reactform/Simpleform"
 import SimpleFormvalidation from './../../components/Reactform/SimpleFormvalidation';
 import Reacthookform from "@/components/Reactform/Reacthookform";
-
+import Form from "@/components/Reactform/Form";
 const page = () => {
   return (
     <div className="min-h-screen bg-gray-900 p-4 border-2  rounded">
         <Simpleform />
         <SimpleFormvalidation />
        <Reacthookform />
+       <Form />
     </div>
   )
 }
