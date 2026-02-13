aa954c6262c3e62fe8b427e9dc1d0176a0ed6ea6
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-13 12:03:21 +0300

    add error handling in react js

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 // app/page.tsx
 import { ErrorBoundary } from "@/components/ErrorBoundary";
 import BuggyCounter from "@/components/BuggyCounter";
-
+import BuggyCounter1 from "@/components/BuggyCounter";
 export default function Page() {
   return (
-    <ErrorBoundary>
-      <BuggyCounter />
-    </ErrorBoundary>
+     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <section>
+      <ErrorBoundary>
+        <BuggyCounter />
+      </ErrorBoundary>
+      </section>
+      <section>
+         <ErrorBoundary>
+          <BuggyCounter1 />
+        </ErrorBoundary>
+      </section>
+    </div>
   );
 }
