b450a3543ea67aa9cb2af29a6c00adb9488a6c28
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-19 16:54:18 +0300

    add hide and show feature

--- app/components/hooks/usereducer/Usereducer.tsx
+++ app/components/hooks/usereducer/Usereducer.tsx
@@ -1,7 +1,8 @@
 "use client"
 
 const initial =0
-import { useReducer } from "react"
+import { useReducer,useState} from "react"
+
 const reducer=(state: number,action: { type: string })=>{
     switch(action.type){
         case "DECREMENT":
@@ -21,9 +22,16 @@ const reducer=(state: number,action: { type: string })=>{
 
 }
 const Reducer=()=>{
+    const [show,setShow] = useState(false)
     const [state,dispatch] = useReducer(reducer,initial);
+    const handleshow =()=>{
+        setShow(show =>!show)
+    }
     return(
         <div className="min-h-screen bg-indigo-900 text-white flex justify-center flex-col items-center">
+            {
+                show && 
+           
          <section className="bg-indigo-500  p-4 rounded text-center">
             <h1 className="text-4xl font-bold shadow-md m-4">UseReducer Hook</h1>
             <p className="text-white text-5xl">{state}</p> 
@@ -31,6 +39,10 @@ const Reducer=()=>{
           <button onClick={()=>dispatch({type:"RESET"})} className="bg-teal-600 px-4 py-2 rounded m-2 text-2xl hover:bg-teal-700 text-white">Reset</button>
           <button onClick={()=>dispatch({type:"INCREMENT"})} className="bg-teal-600 px-4 py-2 rounded m-2 text-2xl hover:bg-teal-700 text-white">Increment</button>
        </section>
+}
+       <button onClick={handleshow} 
+       className="bg-pink-800 text-white rounded px-4 py-2"
+       >{show ? "Hide" : "Show"}</button>
         </div>
     )
 
