1556402bc203e1928b24c6a1a09a5a82af022221
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-19 14:00:27 +0300

    add style using css module

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,11 @@
-
-
+// import  Image from "next/image"
+import styles from "./about.module.css"
 const page = () => {
   return (
     <div className=" flex items-center justify-center bg-indigo-900 text-bold  text-white min-h-screen">
-    <h1>About Page!</h1>
+    <h1 className={styles.title}>About Page!</h1>
+  
+
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import styles from "./about.module.css"
 export default function Page() {
   return (
      
-       <div className=" bg-gray-400 flex items-center justify-center flex-col min-h-screen">
+       <div className=" bg-indigo-700 text-white flex items-center justify-center flex-col min-h-screen">
         <h1 className="text-xl font-bold">Home page</h1>
        
        </div>
