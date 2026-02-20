4235bced9bb58cb573ffd9bc246f6d9a6e87a390
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-20 17:56:45 +0300

    add react-hook-form

---
+++ app/form/page.tsx
@@ -0,0 +1,8 @@
+import Login from "@/app/components/form/Hookform";
+export default function Formpage(){
+    return(
+        <div className="min-h-screen text-white bg-indigo-800 flex justify-center items-center">
+        <Login />              
+        </div>
+    )
+}
