f4abec79abab5b29d6727e7df85820fdffbece5d
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-20 19:25:17 +0300

    add react hook from

--- app/components/form/Name.tsx
+++ app/components/form/Name.tsx
@@ -1,72 +1,37 @@
-//  "use client"
+ "use client"
 
-//   import { useForm } from "react-hook-form";
-//   interface names{
-//     name:string;
-//   }
-
-// const Name = () => {
-//    const {register,handleSubmit,formState:{errors},}=useForm<names>();
-
-//   const onSubmit=(data:names)=>{
-//      console.log(data)
-//   }
-
-//   return (
-//     <div className="flex flex-col items-center">
-//       <form onSubmit={handleSubmit(onSubmit)} className="bg-indigo-600 rounded p-6">
-//         <label htmlFor="name">Name:</label>
-         
-//         <input 
-//           {...register("name",{required:"name required"})}
-//           id="name"
-//           type="text"
-//           placeholder="enter name"
-//         />
-//         {name.errors && <p className="text-red-500">{name.errors.message}</p>}
-
-//         <button type="submit" className="bg-teal-800 text-white px-4 py-2 rounded text-3xl">submit</button>
-//       </form>
-//     </div>
-//   )
-// }
-
-// export default Name
-
-
-
-
-
-"use client";
-
-import { useForm } from "react-hook-form";
-
-interface Names {
-  name: string;
-}
+  import { useForm } from "react-hook-form";
+  interface names{
+    name:string;
+  }
 
 const Name = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm<Names>();
+   const {register,handleSubmit,formState:{errors},}=useForm<names>();
 
-  const onSubmit = (data: Names) => {
-    console.log(data);
-  };
+  const onSubmit=(data:names)=>{
+     console.log(data)
+  }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
+    <div className="flex flex-col items-center">
+      <form onSubmit={handleSubmit(onSubmit)} className="bg-indigo-600 rounded p-6">
         <label htmlFor="name">Name:</label>
-        <input
-          {...register("name", { required: "Name required" })}
+         
+        <input 
+          {...register("name",{required:"name required"})}
           id="name"
           type="text"
-          placeholder="Enter name"
+          placeholder="enter name"
         />
-        {errors.name && <p className="text-red-500">{errors.name.message}</p>}
-        <button type="submit" className="bg-teal-600">add</button>
+        { errors.name && (<p className="text-red-500">{errors.name.message}</p>)}
+
+        <button type="submit" className="bg-teal-800 text-white px-4 py-2 rounded ">submit</button>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default Name;
+export default Name
+
+
+
