00fabcb754d7c94d0eef3f5576ecbd1006cd22e9
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-24 10:33:03 +0300

    add image in the next app

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,16 @@
-// import  Image from "next/image"
-import styles from "./about.module.css"
+import  Image from "next/image"
+
 const AboutPage
  = () => {
   return (
-    <div className=" flex items-center justify-center bg-indigo-900 text-bold  text-white min-h-screen">
-    <h1 className={styles.title}>About Page!</h1>
-  
+    <div className=" flex items-center justify-between p-4 bg-indigo-900 text-bold  text-white min-h-screen">
+    <h1>About Page!</h1>
+    <Image src="/surafel.JPG"
+    alt="no image here"
+   width={200}
+   height={200}
+    className="rounded-full"
+    />
 
     </div>
   )
