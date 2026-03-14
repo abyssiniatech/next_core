6a690140b97596980fd31838f718aa5b91f03a96
abyssiniatech <surafelmengist2015@gmail.com>
2026-03-14 16:36:35 +0300

    add active navlink

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,9 @@
-
+import Adminheader from "../components/Adminheader"
 
 const page = () => {
   return (
     <div className="flex items-center justify-center min-h-screen text-white bg-indigo-800">
+        <Adminheader />
         <h1>Admin page!</h1> 
     </div>
   )
