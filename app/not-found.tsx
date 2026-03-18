755e68d96013a0f027dbfb469ec8ba15634348df
abyssiniatech <surafelmengist2015@gmail.com>
2026-03-18 15:39:33 +0300

    add not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ export default function NotFound() {
     <div className="min-h-screen flex flex-col justify-center items-center">
       <h1 className="text-red-900 text-3xl">404 | Page Not Found</h1>
       <p className="text-red-600 mt-2">
-        Invalid Url...!
+        somthing happend!
       </p>
     </div>
   );
