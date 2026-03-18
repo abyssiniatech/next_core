6affedfa261252d84b516e7030e58b3998c24c26
abyssiniatech <surafelmengist2015@gmail.com>
2026-03-18 15:19:58 +0300

    add params"

--- app/approuter/page.tsx
+++ app/approuter/page.tsx
@@ -1,6 +1,7 @@
-export default function Approuter(){
+export default async function Approuter(){
+   
     return(
-        <div className="bg-indigo-800 text-white min-h-screen flex flex-col items-center ">
+        <div className="bg-indigo-800 text-white min-h-screen flex flex-col items-center mt-4 ">
             <p>App router</p>
         </div>
     )
