58d7fb3f35a2c22cc0226b575a8ac25d273355a2
abyssiniatech <surafelmengist2015@gmail.com>
2026-03-21 09:49:44 +0300

    add custome layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Page() {
   return (
      
-       <div className=" bg-indigo-700 text-white flex  justify-center flex-col min-h-screen">
+       <div className=" bg-indigo-700 text-white flex flex-col  justify-center flex-col min-h-screen">
         <h1 className="text-xl font-bold">Home page</h1>
         <button>click me</button>
        </div>
