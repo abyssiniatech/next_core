d917f3d38b1dde46a3f5e98467d8db71cadf30e8
abyssiniatech <surafelmengist2015@gmail.com>
2026-03-21 10:50:24 +0300

    add  slug

--- app/dashboard/[slug]/page.tsx
+++ app/dashboard/[slug]/page.tsx
@@ -5,5 +5,5 @@ export default async function DashboardUser({params}:{params: Promise<{slug:stri
     <div className="bg-indigo-900 text-white min-h-screen flex justify-center items-center">
       <h1>Dashboard ID: {slug}</h1>
     </div>
-  );ionk
+  );
 }
