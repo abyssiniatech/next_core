67221b20cce8e636a0dd964e7dcf79496c06dbfb
abyssiniatech <surafelmengist2015@gmail.com>
2026-03-29 13:36:57 +0300

    add  styles

--- app/customer/[slug]/page.tsx
+++ app/customer/[slug]/page.tsx
@@ -3,7 +3,7 @@
 const page = async({params}: {params: Promise<{slug: string}>}) => {
     const {slug} = await params;
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen">
         <h1>welcome to my channel : {slug}</h1>
     </div>
   )
