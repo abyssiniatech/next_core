bbcecfb719bafbf618843113cb0ab65e4102a45a
abyssiniatech <surafelmengist2015@gmail.com>
2026-03-29 12:33:03 +0300

    add custome layout

---
+++ app/card/layout.tsx
@@ -0,0 +1,12 @@
+
+
+const layout = ({children}: {children: React.ReactNode}) => {
+  return (
+    <div>
+        <h1>Card Layout</h1>
+        {children}
+    </div>
+  )
+};
+
+export default layout;
