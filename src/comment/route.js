0b438b0d420f086cf0895885bd6fbe28be3176aa
abyssiniatech <surafelmengist2015@gmail.com>
2026-05-05 13:36:58 +0300

    add the firts route on thunder extention:

---
+++ src/comment/route.js
@@ -0,0 +1,8 @@
+import data from "./data"
+export async function GET(){
+    return new Response(JSON.stringify(data),{
+        headers:{
+            'Content-Type':'application/json'
+        }
+    })
+}
